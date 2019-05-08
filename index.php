21184191d7a960af96f1601dd7c5d15bde881e68
Kumara Raghava <41824578+KumaraRaghava@users.noreply.github.com>
2019-05-08 19:01:53 +0530

    Update index.php

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <html>
 <body>
 
-<h1>DevOps first class, Hey there Devops. Changing again....!!!   Great! checking..  </h1>
+<h1>DevOps first class, Hey there Devops. Changing again....!!!   Great! checking.. Final one  </h1>
 
 <?php
 echo "version :final";
